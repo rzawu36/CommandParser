cce2d45aecf13934eec3be49216992984e37c04c
rzawu <perlic.stefan@gmail.com>
2017-10-29 22:53:41 +0100

    first and hopefully last commit

--- CommandParser/Parser.cs
+++ CommandParser/Parser.cs
@@ -10,5 +10,25 @@ namespace CommandParser
     public class Parser
     {
 
-    }
+        public static List<Command> parseCommand(String stringToParse)
+        {
+            String[] commands = stringToParse.Split(',');
+            List<Command> cmds = new List<Command>();
+
+            
+            foreach (String word in commands)
+            {
+                char[] delimiterChars = { 'l', 'r', 'u', 'd' };
+                string[] words = word.Split(delimiterChars, 2);
+                String direction = word.Substring(0, 1);
+                int distance = Int32.Parse(words[1]);
+                Command c = new Command(direction, distance);
+                cmds.Add(c);
+            }
+
+            return cmds;
+        }
+
+
+        }
 }
--- CommandParser/Program.cs
+++ CommandParser/Program.cs
@@ -10,7 +10,13 @@ namespace CommandParser
     {
         static void Main(string[] args)
         {
+            List<Command> cmds = Parser.parseCommand("l2410,r3234,u32330,d2312");
 
+            foreach (Command cmd in cmds)
+            {
+                Console.WriteLine(cmd.ToString());
+            }
+            Console.ReadLine();
         }
     }
 }
